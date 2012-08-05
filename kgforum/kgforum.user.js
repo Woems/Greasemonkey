3d8883d7659ef09d7db54830c0498df238308224
Woems Home <0190666666@freenet.de>
2012-08-05 19:30:45 +0200

    kgforum: komplett überarbeitet

--- kgforum/kgforum.user.js
+++ kgforum/kgforum.user.js
@@ -62,84 +62,167 @@ function getHost() { return location.host; } // hash, host, hostname, href, path
 /********************************/
 
 var param=location.pathname.match(/\/([a-z]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+))?)?)?)?)?/);
+// Typ    , ForumsID, Bereich, Thread, MaxArtikel, Artikel
+// display, 5       , 2407   , 85377 , 42     , 52
 // http://www.kgforum.org/display_5_2407_85377_42_52.html
 // http://www.kgforum.org/threads_5_2407.html
 if (param && param[1])
 switch(param[1])
 {
-  case 'threads': threads(); break;
-  case 'display': display(); break;
+  case 'threads': threads(param[2], param[3], param[4]); break;
+  case 'display': display(param[2], param[3], param[4], param[5]||0, param[6]||0); break;
   case 'index': break;
   default: alert('KGForum: Parameter 1 unbekannt: '+uneval(param)); break;
 } // switch(param[1])
 
-function threads() // Uebersicht über alle Treads
-{
-} // threads()
+// ***** HILFSFUNKTIONEN *****
+function SetData(ForumsID, Bereich, Thread, Artikel, Key, Value) {
+  var data=deserialize('data',{});
+  if (!data[ForumsID]) data[ForumsID]={};
+  if (!data[ForumsID][Bereich]) data[ForumsID][Bereich]={};
+  if (!data[ForumsID][Bereich][Thread]) data[ForumsID][Bereich][Thread]={};
+  if (!data[ForumsID][Bereich][Thread][Artikel]) data[ForumsID][Bereich][Thread][Artikel]={};
+  data[ForumsID][Bereich][Thread][Artikel][Key]=Value;
+  serialize('data',data);
+}
+function GetData(ForumsID, Bereich, Thread, Artikel, Key, DefValue) {
+  var data=deserialize('data',{});
+  try {
+    return data[ForumsID][Bereich][Thread][Artikel][Key];
+  } catch(e) {
+    return DefValue;
+  }
+}
+function AddData(ForumsID, Bereich, Thread, Key, Artikel) {
+  var data=deserialize('data',{});
+  if (!data[ForumsID]) data[ForumsID]={};
+  if (!data[ForumsID][Bereich]) data[ForumsID][Bereich]={};
+  if (!data[ForumsID][Bereich][Thread]) data[ForumsID][Bereich][Thread]={};
+  if (!data[ForumsID][Bereich][Thread][Key]) data[ForumsID][Bereich][Thread][Key]=[];
+  if (data[ForumsID][Bereich][Thread][Key].indexOf(Artikel)==-1)
+    data[ForumsID][Bereich][Thread][Key].push(Artikel);
+  serialize('data',data);
+}
+function RemoveData(ForumsID, Bereich, Thread, Key, Artikel) {
+  var data=deserialize('data',{});
+  if (!data[ForumsID]) data[ForumsID]={};
+  if (!data[ForumsID][Bereich]) data[ForumsID][Bereich]={};
+  if (!data[ForumsID][Bereich][Thread]) data[ForumsID][Bereich][Thread]={};
+  if (!data[ForumsID][Bereich][Thread][Key]) data[ForumsID][Bereich][Thread][Key]=[];
+  if (data[ForumsID][Bereich][Thread][Key].indexOf(Artikel)!=-1)
+    data[ForumsID][Bereich][Thread][Key].splice(data[ForumsID][Bereich][Thread][Key].indexOf(Artikel), 1);
+  serialize('data',data);
+}
+function IfData(ForumsID, Bereich, Thread, Key, Artikel, DefValue) {
+  var data=deserialize('data',{});
+  try {
+    return data[ForumsID][Bereich][Thread][Key].indexOf(Artikel)!=-1
+  } catch(e) {
+    return DefValue;
+  }
+}
 
-function display() // Einzelner Tread mit den Beitraegen der User
+function ArtikelDisplay(Artikel, Display)
+{
+  var ErsterArtikel=location.pathname.match(/\/([a-z]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+))?)?)?)?)?/)[6] || 0;
+  var Position=Artikel*1-ErsterArtikel*1;
+  $('wThread').rows[Position*3+1].style.display=Display;
+  $('wThread').rows[Position*3+2].style.display=Display;
+  $('wThread').rows[Position*3+3].style.display=Display;
+}
+
+function ArtikelAusblenden(ForumsID, Bereich, Thread, Artikel) {
+  //SetData(ForumsID, Bereich, Thread, Artikel, "hide", true);
+  AddData(ForumsID, Bereich, Thread, "hide", Artikel);
+  ArtikelDisplay(Artikel, "none");
+}
+function ArtikelEinblenden(ForumsID, Bereich, Thread, Artikel) {
+  //SetData(ForumsID, Bereich, Thread, Artikel, "hide", false);
+  RemoveData(ForumsID, Bereich, Thread, "hide", Artikel);
+  ArtikelDisplay(Artikel, "");
+}
+function ArtikelAusgeblendet(ForumsID, Bereich, Thread, Artikel) {
+  //return GetData(ForumsID, Bereich, Thread, Artikel, "hide", false);
+  return IfData(ForumsID, Bereich, Thread, "hide", Artikel, false);
+}
+
+// ***** HAUPTFUNKTIONEN *****
+function threads(ForumsID, Bereich, Thread) // Uebersicht über alle Treads
 {
   $xs("/html/body/center[2]").id="wContent";
-  $xs("id('wContent')/table/tbody/tr/td/table[2]").id='wTread';
+  $xs("id('wContent')/table/tbody/tr/td/table[2]").id='wThreads';
+  $x("id('wThreads')/tbody/tr/td[3]/font/a").forEach(function (a) {
+    var param=a.href.match(/\/([a-z]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+)(?:_([0-9]+))?)?)?)?)?.html$/);
+    var data=deserialize('data',{});
+    if (data[param[2]][param[3]][param[4]]) a.style.color="green";
+    GM_log(param);
+  })
+} // threads()
+
+function display(ForumsID, Bereich, Thread, MaxArtikel, Artikel) // Einzelner Tread mit den Beitraegen der User
+{
+  $xs("/html/body/center[2]").id="wContent";
+  $xs("id('wContent')/table/tbody/tr/td/table[2]").id='wThread';
 
   var Titel=$xs("//font[img[@src='http://www2.forennet.org/images/theme1/folder-3.gif']]").lastChild.textContent;
   document.title="KG-Forum - "+Titel.replace(/^\s*|\s*$/g,"");
 
   if (location.hash=="") $("wContent").scrollIntoView()
-  $x("id('wTread')/tbody/tr/td[@rowspan=3]").forEach(function (ZelleAutor,i) {
+  $x("id('wThread')/tbody/tr/td[@rowspan=3]").forEach(function (ZelleAutor,i) {
     var Name=ZelleAutor.firstChild.name;
-    var AktuellerBeitrag="display_"+param[2]+"_"+param[3]+"_"+param[4]+"_"+(param[6]*1+i)+"_"+(param[6]*1+i);
+    var AktuellerArtikel=Artikel*1+i;
     var Links=createElement('font', { size:1 }, ZelleAutor);
 
     createElement('br', {  }, Links);
-    createElement('a', { href:AktuellerBeitrag+".html#"+Name, innerHTML:"[ Beitrag nach Oben ]" }, Links);
+    createElement('a', { href:["display", ForumsID, Bereich, Thread, Math.max(MaxArtikel, AktuellerArtikel+1), AktuellerArtikel].join('_')+".html#"+Name, innerHTML:"[ Beitrag nach Oben ]" }, Links);
 
     createElement('br', {  }, Links);
-    createElement('a', { href:"display_"+param[2]+"_"+param[3]+"_"+param[4]+"_"+param[5]+"_"+(param[6]*1-1)+".html", innerHTML:"[ zurück ]" }, Links);
-    createElement('a', { href:"display_"+param[2]+"_"+param[3]+"_"+param[4]+"_"+param[5]+"_"+(param[6]*1+1)+".html", innerHTML:"[ weiter ]" }, Links);
+    createElement('a', { href:["display", ForumsID, Bereich, Thread, Math.max(MaxArtikel, AktuellerArtikel-1), AktuellerArtikel-1].join('_')+".html", innerHTML:"[ zurück ]" }, Links);
+    createElement('a', { href:["display", ForumsID, Bereich, Thread, Math.max(MaxArtikel, AktuellerArtikel+1), AktuellerArtikel+1].join('_')+".html", innerHTML:"[ weiter ]" }, Links);
 
     createElement('br', {  }, Links);
     createElement('br', {  }, Links);
-    var next=createElement('a', { href:"#", name:"display_"+param[2]+"_"+param[3]+"_"+param[4]+"_"+(param[6]*1-1)+"_"+(param[6]*1-1), innerHTML:"[ Darüber Sichtbar ]" }, Links);
+    var next=createElement('a', { href:'#'+Name, innerHTML:"[ Darüber Sichtbar ]" }, Links);
     next.addEventListener("click",function(event){
-      var ausgeblendet=deserialize("ausgeblendet",{});
-      if (!ausgeblendet[event.target.name]) alert(event.target.name);
-      ausgeblendet[event.target.name]=false;
-      serialize("ausgeblendet",ausgeblendet);
+      ArtikelEinblenden(ForumsID, Bereich, Thread, AktuellerArtikel-1);
       event.stopPropagation();
       event.preventDefault();
     }, true);
     createElement('br', {  }, Links);
-    var prev=createElement('a', { href:"#", name:"display_"+param[2]+"_"+param[3]+"_"+param[4]+"_"+(param[6]*1+1)+"_"+(param[6]*1+1), innerHTML:"[ Darunter Sichtbar ]" }, Links);
+    var prev=createElement('a', { href:'#'+Name, innerHTML:"[ Darunter Sichtbar ]" }, Links);
     prev.addEventListener("click",function(event){
-      var ausgeblendet=deserialize("ausgeblendet",{});
-      if (!ausgeblendet[event.target.name]) alert(event.target.name);
-      ausgeblendet[event.target.name]=false;
-      serialize("ausgeblendet",ausgeblendet);
+      ArtikelEinblenden(ForumsID, Bereich, Thread, AktuellerArtikel+1);
       event.stopPropagation();
       event.preventDefault();
     }, true);
+    /*/
+    createElement('br', {  }, Links);
+    var prev=createElement('a', { href:'#'+Name, innerHTML:"[ ALLE Sichtbar ]" }, Links);
+    prev.addEventListener("click",function(event){
+      var data=deserialize('data',{});
+      data[ForumsID][Bereich][Thread]={};
+      serialize('data',data);
+      location.reload();
+      event.stopPropagation();
+      event.preventDefault();
+    }, true);
+    /**/
 
     createElement('br', {  }, Links);
     createElement('br', {  }, Links);
-    var hide=createElement('a', { href:AktuellerBeitrag+".html", innerHTML:"[ ausblenden ]" }, Links);
+    var hide=createElement('a', { href:'#'+Name, innerHTML:"[ ausblenden ]" }, Links);
     hide.addEventListener("click",function(event){
-      $x('../../.. | ../../../following-sibling::tr[1] | ../../../following-sibling::tr[2]',event.target).forEach(function (e) {
-        e.style.display="none"
-      });
-
-      var ausgeblendet=deserialize("ausgeblendet",{});
-      ausgeblendet[AktuellerBeitrag]=true;
-      serialize("ausgeblendet",ausgeblendet);
-
+      ArtikelAusblenden(ForumsID, Bereich, Thread, AktuellerArtikel);
       event.stopPropagation();
       event.preventDefault();
     }, true);
-    var ausgeblendet=deserialize("ausgeblendet",{});
-    if (ausgeblendet[AktuellerBeitrag])
+
+    // Artikel ausblenden
+    if (ArtikelAusgeblendet(ForumsID, Bereich, Thread, AktuellerArtikel))
       $x('.. | ../following-sibling::tr[1] | ../following-sibling::tr[2]',ZelleAutor).forEach(function (e) {
         e.style.display="none"
       });
+    
   });
 } // display()
 
