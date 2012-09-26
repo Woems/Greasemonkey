5afa04b4f3469a317456a7a33cf6a84da320693c
Woems <0190666666@freenet.de>
2012-09-26 11:15:25 +0200

    Kinopolis: bei abgelaufenen Vorstellungen keinen Link anzeigen

--- kinopolis_-_uebersichtli/kinopolis_-_uebersichtli.user.js
+++ kinopolis_-_uebersichtli/kinopolis_-_uebersichtli.user.js
@@ -167,7 +167,7 @@ $("wUhrzeiten").innerHTML=FilmeNachZeit.filter(function (e) {
   //return e.Film.Status['cool'] && !e.Film.Status['seen'] && !e.Film.Status['del'] && (e.Tag==5 || e.Tag==e.Film.Today);
 }).map(function (e) {
   var Stunde=e.Zeit.substr(0,2)*1;
-  return (19<=Stunde && Stunde<=20?"<b>":"")+w2t[e.Tag]+" "+e.Zeit+" ("+Math.floor(e.Film.DayDiff/7+1)+". Wo)<a href='"+e.Link+"' oldhref='#img_"+e.Film.Nr+"' target=_blank> "+e.Film.Titel+"</a>"+(19<=Stunde && Stunde<=20?"</b>":"");
+  return (19<=Stunde && Stunde<=20?"<b>":"")+w2t[e.Tag]+" "+e.Zeit+" ("+Math.floor(e.Film.DayDiff/7+1)+". Wo)"+(/- *leer *-/.test(e.Link)?e.Film.Titel:"<a href='"+e.Link+"' oldhref='#img_"+e.Film.Nr+"' target=_blank> "+e.Film.Titel+"</a>")+(19<=Stunde && Stunde<=20?"</b>":"");
 }).join("<br>");
 onClick($("wShowAll"),function (e) {
   $("wUebersicht").className=($("wUebersicht").className=='onlycool')?'':'onlycool';
