bbd6c78f46acce454af2cb68342506c5493accc7
Woems Home <0190666666@freenet.de>
2013-04-08 22:54:51 +0200

    H0-Modelbahnforum: Speichern in die MySQL Datenbank geht.

--- H0_Modelbahnforum/H0_Modelbahnforum.user.js
+++ H0_Modelbahnforum/H0_Modelbahnforum.user.js
@@ -322,7 +322,8 @@ function thread() {
   gelesen(ID);
   var Anfang=(getParam("showpage", 1)*1-1)*20;
   var Ende=Anfang+Eintraege.length;
-  if (gelesenbis(ID, Ende)) lastpid(ID,$x("//a[contains(@name,'pid')]").pop().name);  
+  if (gelesenbis(ID, Ende)) lastpid(ID,$x("//a[contains(@name,'pid')]").pop().name);
+  JSONkat(ID,$x("//a[contains(@name,'pid')]").pop().name,Ende);
   //alert([getParam("showpage", 1), Anfang, Ende, ID, uneval(deserialize("data",{})[ID])].join("\n"));
   //alert(uneval(deserialize("data",{})[ID]));
   showmsg({
@@ -336,8 +337,8 @@ function thread() {
     color:(!data[ID] || data[ID].gut==undefined)?"lightgray":(data[ID].gut)?"green":"red",
     OK:"Ja",
     Cancel:"Nein",
-    onOK:function () { gut(ID); },
-    onCancel:function () { schlecht(ID); },
+    onOK:function () { gut(ID); JSONquali(ID,2); },
+    onCancel:function () { schlecht(ID); JSONquali(ID,5); },
     Timeout:(!data[ID] || data[ID].gut==undefined)?120:10,
     onTimeout:function () { },
     //Cancel:"Abbrechen",
