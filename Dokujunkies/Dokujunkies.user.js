1bca3a30aba4ff5581ca003af5d03c909c88b6aa
Woems Home <0190666666@freenet.de>
2015-02-13 13:48:08 +0100

    Dokujunkies hinzugefügt

---
+++ Dokujunkies/Dokujunkies.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Dokujunkies
+// @namespace   Woems
+// @include     http://dokujunkies.org*
+// @version     1
+// ==/UserScript==
+
+document.title=document.title.replace(/Dokujunkies . /,"Doku: ").replace(/Doku: Video2Brain: /,"V2B: ");
