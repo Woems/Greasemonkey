203eb892ed00d52dd262740906e1e5a8effb751d
Woems <0190666666@freenet.de>
2015-06-27 17:27:19 +0200

    Amazon Script hinzugefügt

---
+++ Amazon/Amazon.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name        Amazon
+// @namespace   Woems
+// @include     http://www.amazon.de/*
+// @version     1
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_log
+// @grant       GM_getResourceURL
+// @require     http://code.jquery.com/jquery-1.11.3.min.js
+// @require     https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js
+// @resource    BootstrapCSS https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css
+// @resource    BootstrapThemeCSS https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css
+// ==/UserScript==
+
+
+/******** BASE FUNCTIONS ********/
+function $xs(xpath, rootdir) {return document.evaluate(xpath, rootdir || document, null, 9, null).singleNodeValue;}
+function $x(p, context) {
+  p=p.replace(/([^.])\.(\w+)/g,'$1[@class="$2"]').replace(/#(\w+)/g,'[@id="$1"]').replace(/\/\[/g,'/*[');  // '//.class' '//#id' '//[...]'
+  var i, arr = [], xpr = document.evaluate(p, context || document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+  for (i = 0; item = xpr.snapshotItem(i); i++) arr.push(item);
+  return arr;
+}
+function deserialize(name, def) { return eval(GM_getValue(name, (def || '({})'))); }
+function serialize(name, val) { GM_setValue(name, uneval(val)); }
+function inFrame() { return self!=top; }
+
+
+var l=["href", "protocol", "host", "hostname", "port", "pathname", "hash", "search"];
+//alert(l.map(function (e) { return "location."+e+": "+eval("location."+e); }).join("\n"));
+
+
+
+function CSSbyURL(url) { jQuery("<link rel='stylesheet' type='text/css'>").appendTo(document.getElementsByTagName("HEAD")[0])[0].href=url; }
+function CSSbyRES(res) { CSSbyURL(GM_getResourceURL(res)); }
+
+
+try {
+  //CSSbyRES("BootstrapCSS");
+  //CSSbyRES("BootstrapThemeCSS");
+
+
+function Amazon()
+{
+  this.ASIN="";
+  try {
+  this.ASIN=(($xs('//li[b[contains(text(),"ASIN:")]]')||{textContent:""}).textContent.match(/[A-Z0-9]{10}/)||[""])[0];
+  } catch(e) { alert("ASIN: "+e); }
+  this.data=deserialize("data",{});
+  this.getStatus=function ()
+  {
+    if (!this.data.status) return false;
+    return this.data.status[this.ASIN];
+  }
+  this.setStatus=function (newstat)
+  {
+    this.data=deserialize("data",{});
+    if (!this.data.status) this.data.status={};
+    this.data.status[this.ASIN]=newstat;  
+    serialize("data",this.data);
+  }
+}
+
+if (!inFrame())
+{
+  var AZ=new Amazon();
+  //alert([inFrame(), AZ.ASIN, AZ.getStatus()].join("\n"));
+  //if (AZ.getStatus()=="open" && confirm("Seen?")) AZ.setStatus("seen");
+  jQuery('<div class="a-content"><div class="a-box"><div class="a-box-inner" id="az-menu"><div id="az-status"></div></div></div></div>').appendTo("#rightCol");
+  jQuery("#az-status").text(AZ.ASIN + ": " + AZ.getStatus());
+  if (!AZ.getStatus()) AZ.setStatus("Seen");
+  jQuery('<button class="btn btn-primary" style="color: blue" type="button">Best</button> ').appendTo("#az-menu");
+  jQuery('<button class="btn btn-success" style="color: green" type="button">Good</button> ').appendTo("#az-menu");
+  jQuery('<button class="btn btn-danger" style="color: red" type="button">Bad</button> ').appendTo("#az-menu");
+  jQuery("#az-menu button").click(function (e) { AZ.setStatus(jQuery(this).text()); jQuery("#az-status").text(AZ.getStatus()); });
+
+}
+
+} catch(e) { alert(uneval(e)); }
+
